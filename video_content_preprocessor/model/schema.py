c4960198a0997f42a5fa4bbfb11f160deb520b3b
Ilker Porsuk <il.po@outlook.de>
2023-11-02 23:02:31 +0100

    Update schema

--- video_content_preprocessor/model/schema.py
+++ video_content_preprocessor/model/schema.py
@@ -6,4 +6,6 @@ class VCP(BaseModel):
     youtube_analytics_api_token: str
     download_root_directory: str
     amount_video_content: int
+    open_ai_org_id: str
+    open_ai_api_token: str
     keywords: list
